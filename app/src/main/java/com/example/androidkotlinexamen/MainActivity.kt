02e6df5eb08e62ab7ff5762dd3d6167c9129765e
Rico <rieffel.christopher.dev@gmail.com>
2025-01-20 17:18:45 +0100

    updates the latest elements and problems with git

--- app/src/main/java/com/example/androidkotlinexamen/MainActivity.kt
+++ app/src/main/java/com/example/androidkotlinexamen/MainActivity.kt
@@ -4,20 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -40,36 +31,79 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun calculatorButtonGrid(): List<List<String>> {
-    val grid = listOf(
-        listOf("%","CE","C","Supp"),
-        listOf("7","8","9","*"),
-        listOf("4","5","6","-"),
-        listOf("1","2","3","+"),
-        listOf("/","0",",","=")
-    )
-    return grid
-}
+fun Calculator(modifier: Modifier = Modifier) {
+    // ---- VARIABLES ---- \\
+    var beforeOperator by remember { mutableStateOf("") }
+    var operator by remember { mutableStateOf("") }
+    var afterOperator by remember { mutableStateOf("") }
+    var displayText by remember { mutableStateOf("0") }
 
-@Composable
-fun layoutButtonGrid() {
+    // ---- LAYOUT ---- \\
     Column(
-        verticalArrangement = Arrangement.spacedBy(10.dp)
+        modifier = modifier.padding(14.dp),
+        verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
-        val calculatorRow = calculatorButtonGrid()
+        // --> Operation Display Screen:
+        CalculatorOperationDisplayScreen(displayText = displayText)
 
-        calculatorRow.forEach { row ->
-            Row(
-                horizontalArrangement = Arrangement.spacedBy(10.dp),
-                modifier = Modifier.fillMaxWidth()
-            ) {
-                row.forEach { button ->
-                    Button(
-                        onClick = { null },
-                        //distributes space proportionally within a Row or Column.
-                        modifier = Modifier.weight(1f)
-                    ) {
-                        Text(button)
+        // --> Button grid:
+        Column(
+            verticalArrangement = Arrangement.spacedBy(10.dp)
+        ) {
+            val calculatorRow = calculatorButtonGrid()
+            calculatorRow.forEach { row ->
+                Row(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    row.forEach { button ->
+                        Button(
+                            onClick = {
+                                when {
+                                    button in listOf("0", "1", "2", "3", "4", "5", "6", "7", "8", "9") -> manageNumberInput(
+                                        button,
+                                        beforeOperator = beforeOperator,
+                                        onChangeBeforeOperator =  { newBeforeOperator -> beforeOperator = newBeforeOperator },
+                                        operator = operator,
+                                        afterOperator = beforeOperator,
+                                        onChangeAfterOperator =  { newAfterOperator -> afterOperator = newAfterOperator },
+                                        displayText = displayText,
+                                        onDisplayTextChange = { newDisplayText -> displayText = newDisplayText }
+                                    )
+                                    button in listOf("+", "-", "*", "/", "%") -> manageOperatorInput(
+                                        button,
+                                        beforeOperator = beforeOperator,
+                                        operator = operator,
+                                        onChangeOperator =  { newOperator -> operator = newOperator },
+                                        afterOperator = beforeOperator,
+                                        displayText = displayText,
+                                        onDisplayTextChange = { newDisplayText -> displayText = newDisplayText }
+                                    )
+                                    button == "=" -> manageEqualInput(
+                                        button,
+                                        beforeOperator = beforeOperator,
+                                        operator = operator,
+                                        afterOperator = beforeOperator,
+                                        displayText = displayText,
+                                        onDisplayTextChange = { newDisplayText -> displayText = newDisplayText }
+                                    )
+                                    button == "C" -> manageClear(
+                                        button,
+                                        beforeOperator = beforeOperator,
+                                        onChangeBeforeOperator =  { newBeforeOperator -> beforeOperator = newBeforeOperator },
+                                        operator = operator,
+                                        onChangeOperator =  { newOperator -> operator = newOperator },
+                                        afterOperator = beforeOperator,
+                                        onChangeAfterOperator =  { newAfterOperator -> afterOperator = newAfterOperator },
+                                        displayText = displayText,
+                                        onDisplayTextChange = { newDisplayText -> displayText = newDisplayText }
+                                    )
+                                }
+                            },
+                            modifier = modifier
+                        ) {
+                            Text(button)
+                        }
                     }
                 }
             }
@@ -78,17 +112,88 @@ fun layoutButtonGrid() {
 }
 
 @Composable
-fun Calculator(modifier: Modifier = Modifier) {
-    // ---- VARIABLES ---- \\
+fun CalculatorOperationDisplayScreen(displayText: String) {
+    Text(
+        text = displayText,
+        fontSize = 30.sp,
+        textAlign = TextAlign.End,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp)
+    )
+}
 
-    // ---- LAYOUT ---- \\
-    Column(
-        modifier = modifier.padding(14.dp),
-        verticalArrangement = Arrangement.spacedBy(16.dp)
-    ) {
-        // --> Button grid
-        layoutButtonGrid()
+@Composable
+fun calculatorButtonGrid(): List<List<String>> {
+    return listOf(
+        listOf("%", "CE", "C", "Supp"),
+        listOf("7", "8", "9", "*"),
+        listOf("4", "5", "6", "-"),
+        listOf("1", "2", "3", "+"),
+        listOf("/", "0", ",", "=")
+    )
+}
+
+fun manageNumberInput(
+    button: String,
+    beforeOperator: String,
+    onChangeBeforeOperator: (String) -> Unit,
+    afterOperator: String,
+    onChangeAfterOperator: (String) -> Unit,
+    operator: String,
+    displayText: String,
+    onDisplayTextChange: (String) -> Unit
+) {
+    onChangeBeforeOperator(if (beforeOperator.isEmpty()) button else "")
+    onChangeAfterOperator(if (afterOperator.isEmpty() && operator.isNotEmpty()) button else "")
+    onDisplayTextChange(beforeOperator + operator + afterOperator)
+}
+
+fun manageOperatorInput(
+    button: String,
+    beforeOperator: String,
+    operator: String,
+    onChangeOperator: (String) -> Unit,
+    afterOperator: String,
+    displayText: String,
+    onDisplayTextChange: (String) -> Unit
+) {
+    onChangeOperator(button)
+    onDisplayTextChange(beforeOperator + operator + afterOperator)
+}
+
+fun manageEqualInput(
+    button: String,
+    beforeOperator: String,
+    operator: String,
+    afterOperator: String,
+    displayText: String,
+    onDisplayTextChange: (String) -> Unit
+) {
+    when (operator) {
+        "+" -> onDisplayTextChange((beforeOperator.toInt() + afterOperator.toInt()).toString())
+        "-" -> onDisplayTextChange((beforeOperator.toInt() + afterOperator.toInt()).toString())
+        "*" -> onDisplayTextChange((beforeOperator.toInt() + afterOperator.toInt()).toString())
+        "/" -> onDisplayTextChange((beforeOperator.toInt() + afterOperator.toInt()).toString())
+        "%" -> onDisplayTextChange((beforeOperator.toInt() + afterOperator.toInt()).toString())
     }
+}
+
+fun manageClear(
+    button: String,
+    beforeOperator: String,
+    onChangeBeforeOperator: (String) -> Unit,
+    operator: String,
+    onChangeOperator: (String) -> Unit,
+    afterOperator: String,
+    onChangeAfterOperator: (String) -> Unit,
+    displayText: String,
+    onDisplayTextChange: (String) -> Unit
+) {
+    onChangeBeforeOperator("")
+    onChangeOperator("")
+    onChangeAfterOperator("")
+    onDisplayTextChange("0")
 }
 
 @Preview(showBackground = true)
